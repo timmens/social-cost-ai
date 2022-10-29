173a07992caf874e3c3b6c9045fcdfb0aa4e1747
Tim <tim@mensinger96.com>
2022-10-29 17:43:59 +0200

    Add __init__.py

---
+++ src/scai/server/__init__.py
@@ -0,0 +1,21 @@
+"""Functionality to train a language model from scratch.
+
+This module is adapted from https://tinyurl.com/yz29y9fd.
+
+LICENSE:
+
+Copyright 2020 The HuggingFace Inc. team. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
